5a8eecd302affe72de1a6661063560840dc6c5a1
venkaramana <gubbaramana017@gmail.com>
2020-12-11 16:50:03 +0530

    progess is in progress

--- src/app/core/visit-review/visit-review.component.ts
+++ src/app/core/visit-review/visit-review.component.ts
@@ -52,8 +52,239 @@ export class VisitReviewComponent implements OnInit {
   public getTaskListresponse: Array<any>;
   public getSignaturesresponse: any = {psSignature:'',dcsSignature:''}
   public progressNotes;
-  public formLovList:Array<any>=[]
-  public progressNotesList:Array<any>=[]
+  public formLovList:Array<any>=[{
+    "lovValueId": 2,
+    "displayName": "Female",
+    "lovId": 1,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 1,
+    "value": "Female"
+  }, {
+    "lovValueId": 1,
+    "displayName": "Male",
+    "lovId": 1,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 1,
+    "value": "Male"
+  }, {
+    "lovValueId": 3,
+    "displayName": "Active",
+    "lovId": 2,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 0,
+    "value": "Active"
+  }, {
+    "lovValueId": 4,
+    "displayName": "Inactive",
+    "lovId": 2,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 0,
+    "value": "Inactive"
+  }, {
+    "lovValueId": 5,
+    "displayName": "AP",
+    "lovId": 3,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 1,
+    "value": "AP"
+  }, {
+    "lovValueId": 6,
+    "displayName": "TS",
+    "lovId": 3,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 1,
+    "value": "TS"
+  }, {
+    "lovValueId": 7,
+    "displayName": "India",
+    "lovId": 4,
+    "active": 1,
+    "sortorder": 1,
+    "lovActive": 1,
+    "value": "India"
+  }, {
+    "lovValueId": 8,
+    "displayName": "US",
+    "lovId": 4,
+    "active": 0,
+    "sortorder": 2,
+    "lovActive": 1,
+    "value": "US"
+  }, {
+    "lovValueId": 11,
+    "displayName": "I am still having Issues",
+    "lovId": 6,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 1,
+    "value": "I am still having Issues"
+  }, {
+    "lovValueId": 10,
+    "displayName": "I like what I'm seeing so far",
+    "lovId": 6,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 1,
+    "value": "I like what I'm seeing so far"
+  }, {
+    "lovValueId": 12,
+    "displayName": "What Enhancements",
+    "lovId": 6,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 1,
+    "value": "What Enhancements"
+  }, {
+    "lovValueId": 9,
+    "displayName": "You're crushing it",
+    "lovId": 6,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 1,
+    "value": "You're crushing it"
+  }, {
+    "lovValueId": 13,
+    "displayName": "Whether you are a human resources professiona",
+    "lovId": 7,
+    "active": 0,
+    "sortorder": 0,
+    "lovActive": 1,
+    "value": "Whether you are a human resources professiona"
+  }, {
+    "lovValueId": 14,
+    "displayName": "Whether you are a human resources professional",
+    "lovId": 7,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 1,
+    "value": "Whether you are a human resources professional"
+  }, {
+    "lovValueId": 15,
+    "displayName": "You will also want to be specific and concrete",
+    "lovId": 7,
+    "active": 1,
+    "sortorder": 0,
+    "lovActive": 1,
+    "value": "You will also want to be specific and concrete"
+  }, {
+    "lovValueId": 16,
+    "displayName": "Strongly Agree",
+    "lovId": 8,
+    "active": 1,
+    "sortorder": 1,
+    "lovActive": 1,
+    "value": "Strongly Agree"
+  }, {
+    "lovValueId": 17,
+    "displayName": "Agree",
+    "lovId": 8,
+    "active": 1,
+    "sortorder": 2,
+    "lovActive": 1,
+    "value": "Agree"
+  }, {
+    "lovValueId": 18,
+    "displayName": "Neither agree or disagree",
+    "lovId": 8,
+    "active": 1,
+    "sortorder": 3,
+    "lovActive": 1,
+    "value": "Neither agree or disagree"
+  }, {
+    "lovValueId": 19,
+    "displayName": "Disagree",
+    "lovId": 8,
+    "active": 1,
+    "sortorder": 4,
+    "lovActive": 1,
+    "value": "Disagree"
+  }, {
+    "lovValueId": 20,
+    "displayName": "Strongly Disagree",
+    "lovId": 8,
+    "active": 1,
+    "sortorder": 5,
+    "lovActive": 1,
+    "value": "Strongly Disagree"
+  }]
+  public progressNotesList:Array<any>=[{
+    "surveyDetailsId": 2,
+    "displayType": "textarea",
+    "question": "Comments",
+    "formMappingId": 2,
+    "questionAttributeId": 6,
+    "displayTypeId": 2,
+    "sortOrder": 0,
+    "lovId": 0,
+    "active": 1
+  }, {
+    "surveyDetailsId": 2,
+    "displayType": "selectbox",
+    "question": "Country",
+    "formMappingId": 2,
+    "questionAttributeId": 7,
+    "displayTypeId": 5,
+    "sortOrder": 0,
+    "lovId": 4,
+    "active": 1
+  }, {
+    "surveyDetailsId": 2,
+    "displayType": "textbox",
+    "question": "Enter Your Name",
+    "formMappingId": 2,
+    "questionAttributeId": 3,
+    "displayTypeId": 1,
+    "sortOrder": 0,
+    "lovId": 0,
+    "active": 1
+  }, {
+    "surveyDetailsId": 2,
+    "displayType": "radio",
+    "question": "Gender",
+    "formMappingId": 2,
+    "questionAttributeId": 4,
+    "displayTypeId": 4,
+    "sortOrder": 0,
+    "lovId": 1,
+    "active": 1
+  }, {
+    "surveyDetailsId": 2,
+    "displayType": "checkbox",
+    "question": "States",
+    "formMappingId": 2,
+    "questionAttributeId": 5,
+    "displayTypeId": 3,
+    "sortOrder": 0,
+    "lovId": 3,
+    "active": 1
+  }, {
+    "surveyDetailsId": 2,
+    "displayType": "textbox",
+    "question": "Test1",
+    "formMappingId": 2,
+    "questionAttributeId": 11,
+    "displayTypeId": 1,
+    "sortOrder": 0,
+    "lovId": 0,
+    "active": 1
+  }, {
+    "surveyDetailsId": 2,
+    "displayType": "textbox",
+    "question": "Test3",
+    "formMappingId": 2,
+    "questionAttributeId": 13,
+    "displayTypeId": 1,
+    "sortOrder": 0,
+    "lovId": 0,
+    "active": 1
+  }];
+  public progressmodel:boolean=false;
 
   constructor(public apiservice: ApiserviceService, public modalService: BsModalService, public datepipe: DatePipe) {
     var data = JSON.parse(sessionStorage.getItem("useraccount"));
@@ -182,20 +413,25 @@ export class VisitReviewComponent implements OnInit {
     }
   }
   public getProgressNotes(modalId, departureInfoId) {
+    this.progressmodel=true;
+
     try {
       let obj = { "departureInfoId": departureInfoId }
       this.apiservice.getProgressNotes(JSON.stringify(obj)).subscribe(res => {
         console.log(res)
         let data: any = res;
-        this.progressNotes = data.progressNotes;
-        this.formLovList=data.formLovList;
-        this.progressNotesList=data.progressNotesList;
+        // this.progressNotes = data.progressNotes;
+        // this.formLovList=data.formLovList;
+        // this.progressNotesList=data.progressNotesList;
         $(modalId).modal({ keyboard: false, backdrop: false }, 'show')
       })
     } catch (error) {
 
     }
 
+  }
+  modelhide(){
+    this.progressmodel=false;
   }
 
   public filtertoggles(flag, event) {
