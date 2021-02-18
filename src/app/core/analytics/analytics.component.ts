216445f55a7a05fac7e937a7bad5e6f56ccdc620
venkaramana <gubbaramana017@gmail.com>
2021-02-18 17:52:56 +0530

    done some changes

--- src/app/core/analytics/analytics.component.ts
+++ src/app/core/analytics/analytics.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./analytics.component.sass']
 })
 export class AnalyticsComponent implements OnInit {
- public MangmentScreens:Array<Boolean>=[true,false,false,false]
-  constructor() { }
+  public MangmentScreens: Array<Boolean> = [true, false, false, false]
+
+  measures = [];
+  measures2 = [];
+  measures3 = [];
+  measures4 = [];
+  public headerString = 'Compliance Mesaures';
+  constructor() {
+    let randomStrings = ['Scheduling -Interim', 'Linking visits', 'New Admission', 'Employee onboarding', 'Reconsoliaton', 'Activity', 'Scheduling -Interim', 'Linking visits', 'New Admission', 'Employee onboarding',];
+    let randomSites = ['RCHC GA AUGUSTA REG(1289)', 'RCHC GA ATHENS REG(1244)', 'RCHC GA EASTMEN REG(1244)', 'RCHC GA MACON REG(1244)', 'RCHC GA VOLDOSTA REG(1238)', 'RCHC GA ALBANY REG(1345)', 'RCHC GA COLUMBUS REG(1789)', 'RCHC GA AIKEN REG(1244)', 'RCHC GA THOMASVILLE REG(1325)', 'RCHC GA AUGUSTA REG(1500)', 'RCHC GA ATHENS REG(1244)', 'RCHC GA EASTMEN REG(1244)']
+    for (let i = 1; i < 21; i++) {
+      let obj = { 'sno': i, process: randomStrings[(Math.random() * 10).toFixed(0)] + i, percentage: (Math.random() * 100).toFixed(2) };
+      let obj2 = { site: randomSites[(Math.random() * 10).toFixed(0)], percentage: (Math.random() * 100).toFixed(2) }
+      let obj3 = { 'sno': i, description: 'Some Dummy Descripton', percentage: (Math.random() * 100).toFixed(2) };
+      let obj4 = { 'sno': i, description: 'Some Dummy Descripton', totals: (Math.random() * 1000).toFixed(0) };
+
+      this.measures.push(obj);
+      this.measures2.push(obj2);
+      this.measures3.push(obj3);
+      this.measures4.push(obj4)
+
+    }
+    console.log(this.measures)
+  }
 
   ngOnInit(): void {
   }
 
+  public onMeasurelevelChange(j, headerstring) {
+    this.MangmentScreens = [false, false, false, false]
+    this.MangmentScreens[j] = true;
+    this.headerString=headerstring;
+
+  }
 }
