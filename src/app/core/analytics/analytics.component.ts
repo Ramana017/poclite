20129646a470934ebc841c700ea6e8e0811bc7bd
venkaramana <gubbaramana017@gmail.com>
2021-02-18 16:31:48 +0530

    conditional fixed

--- src/app/core/analytics/analytics.component.ts
+++ src/app/core/analytics/analytics.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./analytics.component.sass']
 })
 export class AnalyticsComponent implements OnInit {
-
+ public MangmentScreens:Array<Boolean>=[true,false,false,false]
   constructor() { }
 
   ngOnInit(): void {
