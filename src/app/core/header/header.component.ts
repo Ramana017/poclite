9f81beb3986d488bad3ce0ee9dd15fc7e526efac
Deepak <deepakannam1@gmail.com>
2021-02-19 14:41:52 +0530

    changes for navigation

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -7,6 +7,7 @@ import { AppService } from 'src/app/services/app.service';
 import { ApiserviceService } from 'src/app/services/apiservice.service';
 import { ZipcodeService } from 'src/app/services/zipcode.service';
 import { UserdetailsService } from 'src/app/services/userdetails.service';
+declare var $: any;
 
 @Component({
   selector: 'app-header',
@@ -35,7 +36,28 @@ export class HeaderComponent implements OnInit {
       this.userName = this.useraccount.userName;
     }
 
-  }
+      $('.navbar .dropdown-item').on('click', function (e) {
+          var $el = $(this).children('.dropdown-toggle');
+          var $parent = $el.offsetParent(".dropdown-menu");
+          $(this).parent("li").toggleClass('open');
+
+          if (!$parent.parent().hasClass('navbar-nav')) {
+              if ($parent.hasClass('show')) {
+                  $parent.removeClass('show');
+                  $el.next().removeClass('show');
+                  $el.next().css({"top": -999, "left": -999});
+              } else {
+                  $parent.parent().find('.show').removeClass('show');
+                  $parent.addClass('show');
+                  $el.next().addClass('show');
+                  $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+              }
+              e.preventDefault();
+              e.stopPropagation();
+          }
+      });
+
+    }
 
   public logout() {
     sessionStorage.removeItem('useraccount');
