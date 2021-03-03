44abc9e88a05648bc18026a51ef78bba38b3641d
venkaramana <gubbaramana017@gmail.com>
2021-03-03 11:28:44 +0530

    popupFixes

--- src/app/schedulingmodule/assign-dcs/assign-dcs.component.ts
+++ src/app/schedulingmodule/assign-dcs/assign-dcs.component.ts
@@ -1,5 +1,5 @@
-import { OnDestroy } from '@angular/core';
-import { Input,Component, OnInit } from '@angular/core';
+import { OnDestroy, TemplateRef } from '@angular/core';
+import { Input, Component, OnInit } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 declare var $: any;
@@ -10,15 +10,130 @@ declare var $: any;
   styleUrls: ['./assign-dcs.component.sass']
 })
 export class AssignDCSComponent implements OnDestroy, OnInit {
-  public availabilityCheck: boolean=true;
+  public availabilityCheck: boolean = true;
   public scheduleArray: Array<any> = ['Yes', 'No'];
+   public criteriaList:Array<any>=[
+    {
+    "criteriaId": 1,
+    "criteriaName": "Qualifications",
+    "enabled": 0,
+    "scheduleCriteriaId": 301219
+    },
+    {
+    "criteriaId": 2,
+    "criteriaName": "Preferences",
+    "enabled": 1,
+    "scheduleCriteriaId": 301220
+    },
+    {
+    "criteriaId": 3,
+    "criteriaName": "Scheduled DCS",
+    "enabled": 1,
+    "scheduleCriteriaId": 301221
+    },
+    {
+    "criteriaId": 4,
+    "criteriaName": "Previously Served PS",
+    "enabled": 1,
+    "scheduleCriteriaId": 301222
+    },
+    {
+    "criteriaId": 5,
+    "criteriaName": "Shared DCS",
+    "enabled": 1,
+    "scheduleCriteriaId": 301223
+    },
+    {
+    "criteriaId": 6,
+    "criteriaName": "Get Preferred DCS",
+    "enabled": 0,
+    "scheduleCriteriaId": 301224
+    },
+    {
+    "criteriaId": 7,
+    "criteriaName": "Get UnAvailable DCS",
+    "enabled": 1,
+    "scheduleCriteriaId": 301225
+    },
+    {
+    "criteriaId": 8,
+    "criteriaName": "Do-not Use \"Un Preferred DCS\"",
+    "enabled": 0,
+    "scheduleCriteriaId": 301226
+    },
+    {
+    "criteriaId": 9,
+    "criteriaName": "Get Family Serving Family DCS",
+    "enabled": 0,
+    "scheduleCriteriaId": 301227
+    }
+    ]
+  public assignDCSList: Array<any> = [
+    {
+    "dcsId": 14634,
+    "overtimeFlag": 1,
+    "dcsName": "ADAMS, CAMILLE",
+    "termination_compare_flag": ""
+    },
+    {
+    "dcsId": 14635,
+    "overtimeFlag": 0,
+    "dcsName": "ADAMS, JOHN",
+    "termination_compare_flag": ""
+    },
+    {
+    "dcsId": 946,
+    "overtimeFlag": 0,
+    "dcsName": "ADAMS, TAMMY",
+    "termination_compare_flag": ""
+    },
+    {
+    "dcsId": 14619,
+    "overtimeFlag": 0,
+    "dcsName": "ALFORD, ASHLEE",
+    "termination_compare_flag": ""
+    },
+    {
+    "dcsId": 15731,
+    "overtimeFlag": 0,
+    "dcsName": "ATKINSON, JANICE",
+    "termination_compare_flag": ""
+    },
+    {
+    "dcsId": 14638,
+    "overtimeFlag": 0,
+    "dcsName": "AVERY, MANDY",
+    "termination_compare_flag": ""
+    },
+    {
+    "dcsId": 14618,
+    "overtimeFlag": 0,
+    "dcsName": "BANKS, ALLISON",
+    "termination_compare_flag": ""
+    },
+    {
+    "dcsId": 14622,
+    "overtimeFlag": 0,
+    "dcsName": "BANKS, LYNDA",
+    "termination_compare_flag": ""
+    },
+    {
+    "dcsId": 15554,
+    "overtimeFlag": 0,
+    "dcsName": "BARRETT, SHEILA",
+    "termination_compare_flag": ""
+    },
+    {
+    "dcsId": 14659,
+    "overtimeFlag": 0,
+    "dcsName": "BENHAM, PAMELA",
+    "termination_compare_flag": ""
+    }
+    ]
 
-  public assignDCSList:Array<any>=[{"dcsId":14634,"overtimeFlag":1,"dcsName":"ADAMS, CAMILLE","termination_compare_flag":""},{"dcsId":14635,"overtimeFlag":0,"dcsName":"ADAMS, JOHN","termination_compare_flag":""},{"dcsId":946,"overtimeFlag":0,"dcsName":"ADAMS, TAMMY","termination_compare_flag":""},{"dcsId":14619,"overtimeFlag":0,"dcsName":"ALFORD, ASHLEE","termination_compare_flag":""},{"dcsId":15731,"overtimeFlag":0,"dcsName":"ATKINSON, JANICE","termination_compare_flag":""},{"dcsId":14638,"overtimeFlag":0,"dcsName":"AVERY, MANDY","termination_compare_flag":""},{"dcsId":14618,"overtimeFlag":0,"dcsName":"BANKS, ALLISON","termination_compare_flag":""},{"dcsId":14622,"overtimeFlag":0,"dcsName":"BANKS, LYNDA","termination_compare_flag":""},{"dcsId":15554,"overtimeFlag":0,"dcsName":"BARRETT, SHEILA","termination_compare_flag":""},{"dcsId":14659,"overtimeFlag":0,"dcsName":"BENHAM, PAMELA","termination_compare_flag":""}]
-  dummy={"futureterminateddcscolor":"#00FFCC","service":"[ICWPS2] ICWPS2 - ICW PERSONAL SUPPORT","assignDCSList":[{"dcsId":14634,"overtimeFlag":1,"dcsName":"ADAMS, CAMILLE","termination_compare_flag":""},{"dcsId":14635,"overtimeFlag":0,"dcsName":"ADAMS, JOHN","termination_compare_flag":""},{"dcsId":946,"overtimeFlag":0,"dcsName":"ADAMS, TAMMY","termination_compare_flag":""},{"dcsId":14619,"overtimeFlag":0,"dcsName":"ALFORD, ASHLEE","termination_compare_flag":""},{"dcsId":15731,"overtimeFlag":0,"dcsName":"ATKINSON, JANICE","termination_compare_flag":""},{"dcsId":14638,"overtimeFlag":0,"dcsName":"AVERY, MANDY","termination_compare_flag":""},{"dcsId":14618,"overtimeFlag":0,"dcsName":"BANKS, ALLISON","termination_compare_flag":""},{"dcsId":14622,"overtimeFlag":0,"dcsName":"BANKS, LYNDA","termination_compare_flag":""},{"dcsId":15554,"overtimeFlag":0,"dcsName":"BARRETT, SHEILA","termination_compare_flag":""},{"dcsId":14659,"overtimeFlag":0,"dcsName":"BENHAM, PAMELA","termination_compare_flag":""}],"overtimevisitcolor":"#CC3399","payorPlan":"1","pastterminateddcscolor":"#33FFFF"}
-
-@Input() dcsObject:any;
-  constructor(public modelService:BsModalService,public bsmodelRef: BsModalRef) { }
- public modalRef: BsModalRef;
+  @Input() dcsObject: any;
+  constructor(public modelService: BsModalService, public bsmodelRef: BsModalRef) { }
+  public modalRef: BsModalRef;
 
 
   ngOnInit(): void {
@@ -35,16 +150,32 @@ export class AssignDCSComponent implements OnDestroy, OnInit {
   dropdown() {
     $(".dashboard-nav-dropdown").toggleClass("show");
   }
-  public openCriteria(){
+  public openCriteria() {
     console.log("open criteria")
 
   }
 
-  public modelHide(){
+  public modelHide() {
     this.bsmodelRef.hide()
   }
+public openschedleTemplate(schedleTemplate:TemplateRef<any>){
+  this.modalRef=this.modelService.show(schedleTemplate,
+    Object.assign({}, { class: 'modal-sm scheduleCriteria modalcontent-wrapper' })
+    )
+}
+public openMarginProtection(schedleTemplate:TemplateRef<any>){
+  console.log("hiii")
+  this.modalRef=this.modelService.show(schedleTemplate,
+    Object.assign({}, { class: ' modalcontent-wrapper modal-dialog-centered edit-modal-content' })
+    )
+}
+public openPaydetails(schedleTemplate:TemplateRef<any>){
+  this.modalRef=this.modelService.show(schedleTemplate,
+    Object.assign({}, { class: ' paydetails-container modal-dialog-centered edit-modal-content  modalcontent-wrapper' })
+    )
+}
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     console.log('++++++++++++++++')
     console.log("NgOndestroy in assign DCS")
   }
