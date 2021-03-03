3d91e5d17e11fa299c8f7d81f1a5ad31e2d93ff1
venkaramana <gubbaramana017@gmail.com>
2021-03-03 10:17:16 +0530

    sharedModule Created

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDatepickerModule.forRoot(),
+  ],
+  exports:[
+    BsDatepickerModule
+
+  ]
+})
+export class SharedModule { }
