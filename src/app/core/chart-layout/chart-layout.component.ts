00b996668df83d14fe19a2f93049d6682389178e
sanjayverma <sanjay.ahex@gmail.com>
2021-03-17 22:25:02 +0530

    bar graph started

--- src/app/core/chart-layout/chart-layout.component.ts
+++ src/app/core/chart-layout/chart-layout.component.ts
@@ -1,122 +1,198 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-declare var $:any;
+import { DashboardService } from 'src/app/services/dashboard.service';
+declare var $: any;
 @Component({
   selector: 'app-chart-layout',
   templateUrl: './chart-layout.component.html',
   styleUrls: ['./chart-layout.component.sass']
 })
 export class ChartLayoutComponent implements OnInit {
-  public siteList= [ {
-    "siteCode": "72072",
-    "siteId": 64,
-    "siteName": "CN COT ADOPTION SUBSIDY"
-    },
-    {
-    "siteCode": "72087",
-    "siteId": 52,
-    "siteName": "CN COT NARBHA"
-    },
-    {
-    "siteCode": "72013",
-    "siteId": 55,
-    "siteName": "CN COTTONWOOD DDD"
-    },
-    {
-    "siteCode": "72014",
-    "siteId": 56,
-    "siteName": "CN COTTONWOOD EVERCARE"
-    },
+  public dropdownSettings = {
+    singleSelection: false,
+    text: "Code",
+    selectAllText: 'Select All',
+    unSelectAllText: 'UnSelect All',
+    enableSearchFilter: true,
+    // enableFilterSelectAll	:true,
+    noDataLabel: "No Data Available",
+    classes: "myclass custom-class",
+    showCheckbox: true,
+    labelKey: "siteCode",
+    // limitSelection: 3,
+    primaryKey: 'siteCode',
+    escapeToClose: false,
+    searchBy: ['siteCode'],
+    position: 'top'
+
+  };
+  public dropdownSettings2 = {
+    singleSelection: false,
+    text: "CSS",
+    selectAllText: 'Select All',
+    unSelectAllText: 'UnSelect All',
+    enableSearchFilter: true,
+    // enableFilterSelectAll	:true,
+    noDataLabel: "No Data Available",
+    classes: "myclass custom-class",
+    showCheckbox: true,
+    labelKey: "siteCode",
+    // limitSelection: 3,
+    primaryKey: 'siteCode',
+    escapeToClose: false,
+    searchBy: ['siteCode'],
+    position: 'top'
+
+  };
+  public dropdownSettingsFilter = {
+    singleSelection: false,
+    text: "Code",
+    selectAllText: 'Select All',
+    unSelectAllText: 'UnSelect All',
+    enableSearchFilter: true,
+    // enableFilterSelectAll	:true,
+    noDataLabel: "No Data Available",
+    classes: "myclass custom-class",
+    showCheckbox: true,
+    labelKey: "site",
+    // limitSelection: 3,
+    primaryKey: 'site',
+    escapeToClose: false,
+    searchBy: ['site'],
+    position: 'top'
+
+  };
+
+  public siteList = [];
+  public officeIds = []
+
+  public archiveDate: Date = new Date();
+  public scheduleStart: Date = new Date();
+  public scheduleEnd: Date = new Date();
+
+  public psFilter=[];
+  public dcsFilter=[];
+  public siteFilter=[];
+  public cssFilter=[];
+  public serviceFilter=[];
+  public visitData=[];
 
 
 
-    ]
-    public selectsite=[]
-    public dropdownSettings = {
-      singleSelection: false,
-      text: "Code",
-      selectAllText: 'Select All',
-      unSelectAllText: 'UnSelect All',
-      enableSearchFilter: true,
-      // enableFilterSelectAll	:true,
-      noDataLabel: "No Data Available",
-      classes: "myclass custom-class",
-      showCheckbox: true,
-      labelKey: "siteCode",
-      // limitSelection: 3,
-      primaryKey: 'siteCode',
-      escapeToClose: false,
-      searchBy: ['siteCode'],
-      position: 'top'
+  private defaultstartdate: Date = new Date();
 
-    };
-    public dropdownSettings2 = {
-      singleSelection: false,
-      text: "CSS",
-      selectAllText: 'Select All',
-      unSelectAllText: 'UnSelect All',
-      enableSearchFilter: true,
-      // enableFilterSelectAll	:true,
-      noDataLabel: "No Data Available",
-      classes: "myclass custom-class",
-      showCheckbox: true,
-      labelKey: "siteCode",
-      // limitSelection: 3,
-      primaryKey: 'siteCode',
-      escapeToClose: false,
-      searchBy: ['siteCode'],
-      position: 'top'
-
-    };
-  single: any[];
+  public cancelledVisitsCount:number=0;
+  public missedVisitsCount:number=0;
+  public openVisitsCount :number=0;
+  single = [{
+    "name": "10/28/2020",
+    "value": 9
+  },
+  {
+    "name": "10/29/2020",
+    "value": 50
+  },];
   multi: any[];
   animationState = 'out';
   view: any[] = [700, 400];
 
   // options
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = false;
-  showXAxisLabel = true;
-  xAxisLabel = 'Date';
-  showYAxisLabel = true;
-  yAxisLabel = 'count';
 
-  public displayVisitCards:boolean=true;
-  public displyHighlightCard='';
+
+  public displayVisitCards: boolean = true;
+  public displyHighlightCard = '';
+  public highlightcardcount:number=0;
 
   colorScheme = {
     domain: ['#4472c3']
   };
 
+  public userData: any;
 
-  constructor() {
-    Object.assign(this, { single })
-
-   }
-  public psList:Array<any>=[];
-  public pskeyword:string='';
-  public tableData:Array<any>=[];
-  ngOnInit(): void {
+  constructor(private dashboardService: DashboardService,public datePipe:DatePipe) {
+    this.userData = JSON.parse(sessionStorage.getItem('useraccount'));
+    this.defaultstartdate.setDate(new Date().getDate() - 7);
+    this.scheduleStart.setDate(new Date().getDate() - 7);
+    // Object.assign(this.single, { single2 })
 
   }
-  visits(){
+  public psList: Array<any> = [];
+  public pskeyword: string = '';
+  public tableData: Array<any> = [];
+
+  ngOnInit(): void {
+    this.getFilterData();
+    this.getDashBoardVisitsCount();
+  }
+  visits() {
     $('.divA').toggleClass('hide');
-    this.displayVisitCards=true;
+    this.displayVisitCards = true;
   }
 
   onSelect(event) {
     console.log(event);
   }
 
+  public getDashBoardVisitsCount() {
 
-  public openvisits(str){
-  this.displyHighlightCard=str;
-  this.displayVisitCards=false;
+    console.log("$$$$$$$$$$$$$########",this.officeIds.toString())
+    let officelist=[];
+    this.officeIds.map(y=>{
+      officelist.push(y.siteId);
+    })
+    let obj = { "userId": this.userData.userId, "startDate": this.datePipe.transform(this.scheduleStart,'MM/dd/yyyy'), "endDate": this.datePipe.transform(this.scheduleEnd,'MM/dd/yyyy'), "officeIds": officelist.length>0?officelist.toString():'' };
+
+    try {
+      this.dashboardService.getDashBoardVisitsCount(JSON.stringify(obj)).subscribe(res => {
+       this.cancelledVisitsCount=res.cancelledVisitsCount;
+       this.openVisitsCount=res.openVisitsCount;
+       this.missedVisitsCount=res.missedVisitsCount;
+      })
+    } catch (error) {
+
+    }
+  }
+
+  public getFilterData() {
+    try {
+      this.dashboardService.getFilterData(this.userData.userId).subscribe(res => {
+        console.log(res);
+        this.siteList = res.siteList
+        this.archiveDate = new Date(res.archiveDate);
+      })
+    } catch (error) {
+
+    }
+  }
+
+  public getDashBoardVisitsDetails(str,count) {
+    this.displyHighlightCard = str;
+    this.displayVisitCards = false;
+    this.highlightcardcount=count;
+   let  officelist=[];
+    this.officeIds.map(y=>{
+      officelist.push(y.siteId);
+    })
+let jsonObj={"userId" : this.userData.userId,"startDate":this.datePipe.transform(this.scheduleStart,'MM/dd/yyyy'),"endDate":this.datePipe.transform(this.scheduleEnd,'MM/dd/yyyy'),"officeIds":officelist.length>0?officelist.toString():'',"visitScenario":str=='Open Visits'?'openvisits':str=='Missed Visits'?'missedvisits':'cancelledvisits'  }
+    try {
+      this.dashboardService.getDashBoardVisitsDetails(JSON.stringify(jsonObj)).subscribe(res=>{
+        console.log(res);
+         this.visitData=res.visitData;
+        this.siteFilter=[...new Set(this.visitData.map(x=> x.site))]
+        this.dcsFilter=[...new Set(this.visitData.map(x=>x.dcsName))]
+        this.cssFilter=[...new Set(this.visitData.map(x=>x.cssName))]
+        this.serviceFilter=[...new Set(this.visitData.map(x=>x.serviceCode))]
+
+        console.log(this.siteFilter)
+
+      })
+    } catch (error) {
+
+    }
 
   }
 }
-export var single = [
+export var single2 = [
   {
     "name": "10/28/2020",
     "value": 9
@@ -158,3 +234,14 @@ export var single = [
     "value": 68
   }
 ];
+
+
+
+// showXAxis = true;
+//   showYAxis = true;
+//   gradient = false;
+//   showLegend = false;
+//   showXAxisLabel = true;
+//   xAxisLabel = 'Date';
+//   showYAxisLabel = true;
+//   yAxisLabel = 'count';
