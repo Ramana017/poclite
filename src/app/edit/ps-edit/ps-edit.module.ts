e793b73e4303fce24508e69fd5e69b5e82b192d9
saichand1234 <saichand@ahex.co.in>
2021-03-18 12:20:02 +0530

    Added changes in ui

--- src/app/edit/ps-edit/ps-edit.module.ts
+++ src/app/edit/ps-edit/ps-edit.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { PsEditRoutingModule } from './ps-edit-routing.module';
 import { PsEditComponent } from './ps-edit.component';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-
+import {FileUploadModule} from 'primeng/fileupload';
+import {HttpClientModule} from '@angular/common/http';
+import { ModalModule } from 'ngx-bootstrap/modal';
 @NgModule({
   declarations: [PsEditComponent],
   imports: [
     CommonModule,
     PsEditRoutingModule,
-    TabsModule.forRoot()
+    TabsModule.forRoot(),
+    FileUploadModule,HttpClientModule,
+    ModalModule
   ]
 })
 export class PsEditModule { }
