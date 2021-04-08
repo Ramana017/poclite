c2b14eaa0284f83ca4692ed6c33ebc65acbd1195
saichand1234 <saichand@ahex.co.in>
2021-04-08 20:48:58 +0530

    Added admission changes

--- src/app/admission/admission.module.ts
+++ src/app/admission/admission.module.ts
@@ -5,12 +5,17 @@ import { AdmissionRoutingModule } from './admission-routing.module';
 import { AdmissionComponent } from './admission.component';
 import { EditAdmissionComponent } from './edit-admission/edit-admission.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
-
+import {OverlayPanelModule} from 'primeng/overlaypanel';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {ButtonModule} from 'primeng/button';
+import {CheckboxModule} from 'primeng/checkbox';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import {SplitButtonModule} from 'primeng/splitbutton';
 @NgModule({
   declarations: [AdmissionComponent, EditAdmissionComponent],
   imports: [
     CommonModule,
-    AdmissionRoutingModule,DragDropModule
+    AdmissionRoutingModule,DragDropModule,OverlayPanelModule,MultiSelectModule,ButtonModule,CheckboxModule,BsDropdownModule,SplitButtonModule
   ]
 })
 export class AdmissionModule { }
--- src/app/admission/edit-admission/edit-admission.component.ts
+++ src/app/admission/edit-admission/edit-admission.component.ts
@@ -1,28 +1,72 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {MenuItem} from 'primeng/api';
+interface City {
+  name: string,
+  code: string
+}
+
 @Component({
   selector: 'app-edit-admission',
   templateUrl: './edit-admission.component.html',
   styleUrls: ['./edit-admission.component.sass']
 })
 export class EditAdmissionComponent implements OnInit {
+  cities: City[];
+  items: MenuItem[];
 
+  selectedCities2: City[];
   public modalRef: BsModalRef;
-  constructor(private modalService: BsModalService) {}
+  constructor(private modalService: BsModalService) {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+    ];
+  }
 
   ngOnInit(): void {
+    this.items = [
+      {label: 'Update', icon: 'pi pi-refresh'},
+      {label: 'Delete', icon: 'pi pi-times'},
+      {label: 'Angular.io', icon: 'pi pi-info'},
+      {label: 'Setup', icon: 'pi pi-cog'}
+  ];
   }
-  guarantorlogdata(guarantorlog: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(guarantorlog);
+  ppListdata(ppList: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(ppList);
+  }
+  ppHistorydata(ppHistory: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(ppHistory, Object.assign({}, { class: 'ppHistory modal-dialog-centered' }));
+  }
+  phyHistorydata(phyHistory: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(phyHistory, Object.assign({}, { class: 'ppHistory modal-dialog-centered' }))
+  }
+  digHistorydata(digHistory: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(digHistory, Object.assign({}, { class: ' modal-dialog-centered' }))
+  }
+  phyListdata(phyList: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(phyList, Object.assign({}, { class: 'ppHistory modal-dialog-centered' }))
+  }
+  editdigdata(editdig: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(editdig, Object.assign({}, { class: 'editdig-modal modal-dialog-centered' }))
+  }
+  addDig(diglist:TemplateRef<any>){
+    this.modalRef = this.modalService.show(diglist,Object.assign({},{ class: 'registration-modal-container modal-dialog-centered modal-dialog-scrollable'}))
   }
   movies = [
     'Episode I - The Phantom Menace',
     'Episode II - Attack of the Clones',
-    
+
   ];
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
   }
+
+
+
 }
