43ce816d80b38687983e0e9ac3a2eb4b1ff5b07e
Ramana017 <gubbaramana017@gmail.com>
2021-04-20 19:37:21 +0530

    daily started

--- src/app/core/daily-reports/daily-reports.component.ts
+++ src/app/core/daily-reports/daily-reports.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { DashboardService } from 'src/app/services/dashboard.service';
 
 @Component({
   selector: 'app-daily-reports',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./daily-reports.component.sass']
 })
 export class DailyReportsComponent implements OnInit {
-
+public userData:any;
   public navbuttons:Array<boolean>=[false,true,false];
-  constructor() { }
+  constructor(private dashboardService: DashboardService,private datePipe:DatePipe,private modalService:BsModalService) {
+    this.userData = JSON.parse(sessionStorage.getItem('useraccount'));
+    this.applyjobDate=this.datePipe.transform(this.jobRunDate,'MM/dd/yyyy');
 
+  }
   public dailyReportStats:any={
     "January": [
         {
@@ -9532,7 +9538,133 @@ export class DailyReportsComponent implements OnInit {
     ]
 }
 
+public dailyStatsList=[];
+
   ngOnInit(): void {
   }
+
+public getDailyUtilStats(){
+  try {
+    this.dashboardService.getDailyUtilStats({}).subscribe(res=>{
+      console.log(res);
+      this.dailyStatsList=res.dailyStatsList;
+    })
+  } catch (error) {
+
+  }
+}
+
+
+
+
+
+
+
+
+
+   //filter Related variables and functionality.
+
+   public jobRunDate: Date = new Date();
+   public rvpList = [];
+   public selectedRvpList = [];
+   public edsList = [];
+   public selectedEdList = [];
+   public bmList = [];
+   public selectedBranches = [];
+   public siteList = [];
+   public selectedSites = [];
+   public modelRef:BsModalRef;
+
+
+
+   private appliedRvpList = [];
+   private appliedEdsList = [];
+   private appliedBrancheslist = [];
+   private appliedSitelist = [];
+   private applyjobDate:string='';
+
+   public getRVPList() {
+     let obj = { "userId": this.userData.userId, "userTypeId": 0, "name": "" };
+     try {
+       this.dashboardService.getRVPList(JSON.stringify(obj)).subscribe(res => {
+         console.log(res);
+         this.rvpList = res.rvpList;
+       })
+     } catch (error) {
+
+     }
+   }
+   public getEDList() {
+     this.selectedEdList=[];
+     this.selectedBranches=[];
+     this.selectedSites=[];
+     console.log(this.selectedRvpList.map(x => x.operationOfficer).toString())
+     if (this.selectedRvpList.length > 0) {
+       try {
+         let obj = { "userId": this.userData.userId, "userTypeId": 0, "name": "", "rvpIds": this.selectedRvpList.map(x => x.operationOfficer).toString() }
+
+         this.dashboardService.getEDList(JSON.stringify(obj)).subscribe(res => {
+           console.log(res);
+           this.edsList = res.edList;
+
+         })
+       } catch (error) {
+
+       }
+     }else{
+     this.edsList=[];
+
+     }
+   }
+   public getBMList() {
+     this.selectedBranches=[];
+     this.selectedSites=[];
+     if (this.selectedEdList.length > 0) {
+
+       let obj = { "userId": this.userData.userId, "userTypeId": 0, "name": "", "rvpIds": this.selectedRvpList.map(x => x.operationOfficer).toString(), "edIds": this.selectedEdList.map(x => x.executiveDirector).toString() }
+       try {
+         this.dashboardService.getBMList(JSON.stringify(obj)).subscribe(res => {
+           console.log(res);
+           this.bmList = res.bmList;
+
+         })
+       } catch (error) {
+
+       }
+     }
+   }
+   public getSiteList() {
+     this.selectedSites=[];
+
+     if (this.selectedBranches.length > 0) {
+       let obj = { "userId": this.userData.userId, "userTypeId": 0, "name": "", "rvpIds": this.selectedRvpList.map(x => x.operationOfficer).toString(), "edIds": this.selectedEdList.map(x => x.executiveDirector).toString(), "bmIds": this.selectedBranches.map(x => x.branchManager).toString() }
+       try {
+         this.dashboardService.getSiteList(JSON.stringify(obj)).subscribe(res => {
+           console.log(res);
+           this.siteList = res.siteList;
+
+         })
+       } catch (error) {
+
+       }
+     }
+   }
+
+   public onApply(){
+     this.appliedRvpList= this.selectedRvpList;
+    this.appliedEdsList= this.selectedEdList;
+    this.appliedBrancheslist= this.selectedBranches;
+    this.appliedSitelist= this.selectedSites;
+     this.applyjobDate=this.datePipe.transform(this.jobRunDate,'MM/dd/yyyy');
+     this.getDailyUtilStats();
+     this.modelRef.hide();
+
+   }
+
+
+   public openFilter(template:TemplateRef<any>){
+     this.modelRef=this.modalService.show(template,{class:'',
+   })
+   }
 
 }
