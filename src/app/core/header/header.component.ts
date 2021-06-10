69543a7141c0e2277af48d500b97d031e6969f78
saichand1234 <saichand@ahex.co.in>
2021-06-10 14:49:15 +0530

    Added changes in header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -108,11 +108,17 @@ clickheader(){
   $(".hide-toggle").click(function(){
 $(".dropdown-menu").removeClass("show");
   });
-  $(".side-menu-inner-item").click(function(){
+  $(".side-menu-inner-item", ".parent-item").click(function(){
     // $('.side-menu-inner-item:has(.active)')
     if($(".side-menu-inner-item").hasClass("active")){
       $(".side-menu-item").addClass("active")
     }
+    else{
+      $(".side-menu-item").removeClass("active")
+    }
+  })
+  $( ".parent-item").click(function(){
+      $(".side-menu-item").removeClass("active")
   })
 }
 }
